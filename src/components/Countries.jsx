bd1766af7a2812fe211cfec6563ab27b3af348c5
Ondairos <ondairos@gmail.com>
2022-12-29 14:31:18 +0200

    added single country result

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,7 +4,7 @@ export const Countries = (props) => {
   return (
     <div>
       {props.countriesToShow.length == 1 ? (
-        <p>1 country!</p>
+        <p>{props.countriesToShow[0].name}</p>
       ) : props.countriesToShow.length > 10 ? (
         <p>Search the country you want.</p>
       ) : (
