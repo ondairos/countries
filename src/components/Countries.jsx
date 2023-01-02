ceff1fd77941d2e8e78575c26c28c7c4ad490ce2
Ondairos <ondairos@gmail.com>
2023-01-02 13:16:10 +0200

    added capital, area, languages display to app

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,7 +4,14 @@ export const Countries = (props) => {
   return (
     <div>
       {props.countriesToShow.length == 1 ? (
-        <p>{props.countriesToShow[0].name}</p>
+        <>
+          <p>Name: {props.countriesToShow[0].name}</p>
+          <p>Capital: {props.countriesToShow[0].capital}</p>
+          <p>Area: {props.countriesToShow[0].area}</p>
+          {/* use .languages.map() to extract the name of the language */}
+          <p><b>Languages</b>: {props.countriesToShow[0].languages.map(country => <li>{country.name}</li>)}</p>
+          <img src={props.countriesToShow[0].flags.png} alt="" />
+        </>
       ) : props.countriesToShow.length > 10 ? (
         <p>Search the country you want.</p>
       ) : (
