914117b69d468c5b295c968e4cac8b8de533cd87
Ondairos <ondairos@gmail.com>
2023-01-03 11:21:44 +0200

    added show functionality for multiple countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Countries = (props) => {
+  const [clicked, setClicked] = useState(false);
+  console.log(clicked);
+
+  function handleClick() {
+    return setClicked((prevClicked) => !prevClicked);
+  }
+
   return (
     <div>
       {props.countriesToShow.length == 1 ? (
@@ -9,7 +16,12 @@ export const Countries = (props) => {
           <p>Capital: {props.countriesToShow[0].capital}</p>
           <p>Area: {props.countriesToShow[0].area}</p>
           {/* use .languages.map() to extract the name of the language */}
-          <p><b>Languages</b>: {props.countriesToShow[0].languages.map(country => <li>{country.name}</li>)}</p>
+          <p>
+            <b>Languages</b>:{" "}
+            {props.countriesToShow[0].languages.map((country) => (
+              <li>{country.name}</li>
+            ))}
+          </p>
           <img src={props.countriesToShow[0].flags.png} alt="" />
         </>
       ) : props.countriesToShow.length > 10 ? (
@@ -19,7 +31,24 @@ export const Countries = (props) => {
           {props.countriesToShow.map((element) => {
             return (
               <li key={element.name}>
-                {element.name} :----: {element.capital}
+                {element.name} <button onClick={handleClick}>Show</button>
+                {clicked ? (
+                  <>
+                    <p>Name: {element.name}</p>
+                    <p>Capital: {element.capital}</p>
+                    <p>Area: {element.area}</p>
+                    {/* use .languages.map() to extract the name of the language */}
+                    <p>
+                      <b>Languages</b>:{" "}
+                      {element.languages.map((country) => (
+                        country.name
+                      ))}
+                    </p>
+                    <img src={element.flags.png} alt="" />
+                  </>
+                ) : (
+                  ""
+                )}
               </li>
             );
           })}
